42dc2bdfc7d575d1d3a96d5b8a08f66d85abccef
David Slayback <david.slayback2010@gmail.com>
2018-10-25 13:16:42 -0400

    Added a self-explanation for as I go through recoding this.

---
+++ G-DICE-Python/Domain.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function, division
+
+class Domain(object):
+
+    def __init__(self):
+        self.beliefNodes = [] #List of belief nodes in this domain
+        self.TMAs = [] #List of task macro-actions in this domain
+        self.numTMAs = 0 #Initially no TMAs
+        self.numBeliefNodes = 5 #Number of expected belief nodes
+        self.agents = [] #List of agents in domain
+        self.validTMATransitions
+
+
+    def
