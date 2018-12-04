b4895bc382e259b2c0f74f3a123dc8aee0b01edd
Unknown <david.slayback2010@gmail.com>
2018-12-04 14:28:04 -0500

    ADded additional functions to FSC for multiple samples

--- generalGDICE.py
+++ generalGDICE.py
@@ -2,6 +2,7 @@ from gym_pomdps import list_pomdps
 import gym
 import numpy as np
 import numpy.random as npr
+from multiprocessing import Pool
 
 class FiniteStateController(object):
     def __init__(self, numNodes, numActions, numObservations):
@@ -42,11 +43,21 @@ class FiniteStateController(object):
     def sampleAction(self, numSamples=1):
         return npr.choice(np.arange(self.numActions), size=numSamples, p=self.actionProbabilities[self.currentNode, :])
 
+    # Get an action from all nodes accordding to probability. Can sample multiple actions
+    def sampleActionFromAllNodes(self, numSamples=1):
+        actionIndices = np.arange(self.numActions)
+        return np.array([npr.choice(actionIndices, size=numSamples, p=self.actionProbabilities[nodeIndex,:])
+                         for nodeIndex in range(self.numNodes)],dtype=np.int32)
+
     # Get the next node according to probability given current node and observation index
     # Can sample multiple transitions
     # DOES NOT set the current node
     def sampleObservationTransition(self, observationIndex, numSamples=1):
-        return npr.choice(np.arange(self.numActions), size=numSamples, p=self.actionProbabilities[self.currentNode, :, observationIndex])
+        return npr.choice(np.arange(self.numNodes), size=numSamples, p=self.nodeTransitionProbabilities[self.currentNode, :, observationIndex])
+
+    def sampleObservationTransitionFromAllNodes(self, observationIndex, numSamples=1):
+        nodeIndices = np.arange(self.numNodes)
+        return np.array([npr.choice(nodeIndices, size=numSamples, p=self.nodeTransitionProbabilities[nodeIndex, :, observationIndex]) for nodeIndex in range(self.numNodes)], dtype=np.int32)
 
 
     # Update the probability of taking an action in a particular node
@@ -58,10 +69,61 @@ class FiniteStateController(object):
     def updateTransitionProbability(self, firstNodeIndex, secondNodeIndex, observationIndex, newProbability):
         self.nodeTransitionProbabilities[firstNodeIndex, secondNodeIndex, observationIndex] = newProbability
 
+    # Get the current probability tables
+    def save(self):
+        return self.actionProbabilities, self.nodeTransitionProbabilities
 
+
+# Evaluate controller(s) on an environment, given
+# Inputs:
+#   env: Gym-like environment to evaluate on
+#   controller: A controller or list of controllers corresponding to agents in the environment
+#   params: GDICEParams object
+#   timeHorizon: Number of timesteps to evaluate to. If None, run each sample until episode is finished
+#   parallel: Attempt to use python multiprocessing across samples. If not None, should be a Pool object
+
+def evaluateFSCOnEnvironment(env, controller, params, timeHorizon=None, parallel=None):
+    # Ensure controller matches environment
+    assert env.action_space.n == controller.numActions
+    assert env.state_space.n == controller.numObservations
+
+    # Reset controller
+    controller.reset()
+
+    # Start variables
+    bestActionProbs = None
+    bestNodeTransitionProbs = None
+    bestValue = np.NINF
+    for iteration in params.numIterations:
+        # For each node in controller, sample actions
+        sampledActions = controller.sampleAction
+
+
+    pass
+
+# Evaluate a single sample
+def evaluateSample(env, controller):
+
+
+
+# GDICE parameter object
+# Inputs:
+#   numIterations: N_k number of iterations of GDICE to perform
+#   numSamples: N_s number of samples to take for each iteration from each node
+#   numBestSamples: N_b number of samples to keep from each set of samples
+#   leareningRate: 0-1 alpha value, learning rate at which controller shifts probabilities
+#   valueThreshold: If not None, ignore all samples with worse values, even if that means there aren't numBestSamples
+class GDICEParams(object):
+    def __init__(self, numIterations=30, numSamples=50, numBestSamples=5, learningRate=0.1, valueThreshold=None):
+        self.numIterations = numIterations
+        self.numSamples = numSamples
+        self.numBestSamples = numBestSamples
+        self.learningRate = learningRate
+        self.valueThreshold = valueThreshold
 
 
 
 
 if __name__ == "__main__":
     env = gym.make(list_pomdps()[2])  # 4x3-v0 POMDP
+    pool = Pool()  # Use a pool for parallel processing. Max # threads
