84cdf2c2e2e23c0dea87918283e0eca326bbd62f
Unknown <david.slayback2010@gmail.com>
2018-12-04 17:54:35 -0500

    Creating MultiPOMDP wrapper

--- generalGDICE.py
+++ generalGDICE.py
@@ -1,4 +1,4 @@
-from gym_pomdps import list_pomdps
+from gym_pomdps import list_pomdps, POMDP
 import gym
 import numpy as np
 import numpy.random as npr
@@ -43,11 +43,12 @@ class FiniteStateController(object):
     def sampleAction(self, numSamples=1):
         return npr.choice(np.arange(self.numActions), size=numSamples, p=self.actionProbabilities[self.currentNode, :])
 
-    # Get an action from all nodes accordding to probability. Can sample multiple actions
+    # Get an action from all nodes according to probability. Can sample multiple actions
+    # Outputs numNodes * numSamples
     def sampleActionFromAllNodes(self, numSamples=1):
         actionIndices = np.arange(self.numActions)
         return np.array([npr.choice(actionIndices, size=numSamples, p=self.actionProbabilities[nodeIndex,:])
-                         for nodeIndex in range(self.numNodes)],dtype=np.int32)
+                         for nodeIndex in range(self.numNodes)], dtype=np.int32)
 
     # Get the next node according to probability given current node and observation index
     # Can sample multiple transitions
@@ -55,9 +56,19 @@ class FiniteStateController(object):
     def sampleObservationTransition(self, observationIndex, numSamples=1):
         return npr.choice(np.arange(self.numNodes), size=numSamples, p=self.nodeTransitionProbabilities[self.currentNode, :, observationIndex])
 
+    # Get the next node for each node given observation index
+    # Outputs numNodes * numSamples
     def sampleObservationTransitionFromAllNodes(self, observationIndex, numSamples=1):
         nodeIndices = np.arange(self.numNodes)
-        return np.array([npr.choice(nodeIndices, size=numSamples, p=self.nodeTransitionProbabilities[nodeIndex, :, observationIndex]) for nodeIndex in range(self.numNodes)], dtype=np.int32)
+        return np.array([npr.choice(nodeIndices, size=numSamples, p=self.nodeTransitionProbabilities[nodeIndex, :, observationIndex])
+                         for nodeIndex in range(self.numNodes)], dtype=np.int32)
+
+    # Get the next node for all nodes for all observation indices
+    # Outputs numObs * numNodes * numSamples
+    def sampleAllObservationTransitionsFromAllNodes(self, numSamples=1):
+        obsIndices = np.arange(self.numObservations)
+        return np.array([self.sampleObservationTransitionFromAllNodes(obsIndex, numSamples)
+                         for obsIndex in obsIndices], dtype=np.int32)
 
 
     # Update the probability of taking an action in a particular node
@@ -82,7 +93,7 @@ class FiniteStateController(object):
 #   timeHorizon: Number of timesteps to evaluate to. If None, run each sample until episode is finished
 #   parallel: Attempt to use python multiprocessing across samples. If not None, should be a Pool object
 
-def evaluateFSCOnEnvironment(env, controller, params, timeHorizon=None, parallel=None):
+def evaluateFSCOnEnvironment(env, controller, params, timeHorizon=50, parallel=None):
     # Ensure controller matches environment
     assert env.action_space.n == controller.numActions
     assert env.state_space.n == controller.numObservations
@@ -96,13 +107,19 @@ def evaluateFSCOnEnvironment(env, controller, params, timeHorizon=None, parallel
     bestValue = np.NINF
     for iteration in params.numIterations:
         # For each node in controller, sample actions
-        sampledActions = controller.sampleAction
+        sampledActions = controller.sampleActionFromAllNodes(params.numSamples)
+
+        # For each node, observation in controller, sample next node
+        sampledNodes = controller.sampleAllObservationTransitionsFromAllNodes(params.numSamples)
+
+        # For each sampled action, evaluate in environment
 
 
     pass
 
 # Evaluate a single sample
 def evaluateSample(env, controller):
+    pass
 
 
 
@@ -122,8 +139,42 @@ class GDICEParams(object):
         self.valueThreshold = valueThreshold
 
 
+# States, observations, rewards, actions, dones are now lists or np arrays
+class MultiActionPOMDP(gym.Wrapper):
+
+    def __init__(self, env, numTrajectories):
+        assert isinstance(env, POMDP)
+        super().__init__(env)
+        self.numTrajectories = numTrajectories
+        self.reset()
+
+    def __getattr__(self, attr):
+        return getattr(self.env, attr)
+
+    def reset(self):
+        if self.env.start is None:
+            self.state = self.np_random.randint(
+                self.state_space.n, size=self.numTrajectories)
+        else:
+            self.state = self.np_random.multinomial(
+                1, self.env.start, size=self.numTrajectories).argmax(1)
+
+    # Step given an nparray or list of actions
+    # If actions is a scalar, applies to all
+    def step(self, actions):
+        if np.isscalar(actions):
+            actions = np.full(self.numTrajectories, actions, dtype=np.int32)
+        newStates = np.array([self.np_random.multinomial(1, p).argmax() for p in self.env.T[self.state, actions]])
+        obs =np.array([self.np_random.multinomial(1, p).argmax() for p in self.env.O[self.state, actions, newStates]])
+        rewards = np.array(self.env.R[self.state, actions, newStates, obs])
+        done = np.zeros(self.numTrajectories, dtype=bool)
+        self.state = newStates
+        return obs, rewards, done, {}
 
 
 if __name__ == "__main__":
     env = gym.make(list_pomdps()[2])  # 4x3-v0 POMDP
+    multiEnv = MultiActionPOMDP(env, 50)
+    multiEnv.step(0)
+    controller = FiniteStateController(10, 4, 11)
     pool = Pool()  # Use a pool for parallel processing. Max # threads
