cdb93393746f0cafc97a63bd18c16395a491b6ff
Unknown <david.slayback2010@gmail.com>
2018-12-17 12:52:22 -0500

    Added some dpomdp files. Made a DPOMDP gym environment based on Andrea's dpomdp parser and POMDP environment.
    The DPOMDP environment works on many of the files, but runs into two classes of issues:
    The first is that sometimes the parser fails. I'm going to leave that for now
    The second is that due to rounding, sometimes the transition probabilities of an environment fail to sum to 1 for some permutation. I'm working on a function to normalize the tables

---
+++ DPOMDPScratch.py
@@ -0,0 +1,95 @@
+import os
+from rl_parsers.dpomdp import parse
+import numpy as np
+import gym
+from gym.utils import seeding
+from gym import spaces
+
+class DPOMDP(gym.Env):
+    """Environment specified by DPOMDP file"""
+    def __init__(self, path, episodic=False, seed=None):
+        self.episodic = episodic
+        self.seed(seed)
+        with open(path) as f:
+            model = parse(f.read())
+
+        self.discount = model.discount
+        self.agents = len(model.agents)
+        self.state_space = spaces.Discrete(len(model.states))
+        self.action_space = [spaces.Discrete(len(aAct)) for aAct in model.actions]  # Space for each agent
+        self.observation_space = [spaces.Discrete(len(aObs)) for aObs in model.observations]  # Obs space for each agent
+        self.reward_range = model.R.min(), model.R.max()
+        if model.start is None:
+            self.start = np.full(self.state_space.n, 1 / self.state_space.n)
+        else:
+            self.start = model.start
+
+        # Start-state, agent actions, end state
+        self.T = model.T.transpose(self.agents, *(np.arange(self.agents)), self.agents+1).copy()
+        # Start, agent actions, end, agent observations
+        self.O = np.stack([model.O] * self.state_space.n)
+        # Start-state, agent actions, end state, agent observations
+        self.R = model.R.transpose(self.agents, *(np.arange(self.agents)), self.agents+1, *(np.arange(self.agents)+self.agents+2)).copy()
+
+        if episodic:
+            self.D = model.reset.T.copy()  # only if episodic
+
+        self.state = None
+        #self._normalizeInit()
+
+    def seed(self, seed):
+        self.np_random, seed_ = seeding.np_random(seed)
+        return [seed_]
+
+    def reset(self):
+        self.state = self.np_random.multinomial(1, self.start).argmax().item()
+
+    # Take an nparray or tuple of actions, not just 1
+    def step(self, actions):
+        assert self.state is not None, 'State has not been initialized'
+        assert actions.shape[0] == self.agents, 'Must provide joint action'
+
+        state1 = self.np_random.multinomial(
+            1, self.T[self.state, (*actions)]).argmax().item()
+        # Need to get the joint obs. The last indices are obs for agents 1, 2, etc
+        # Flatten those indices (they sum to 1), draw a number, unflatten
+        # So obs is a tuple of length (numAgents)
+        obs = np.unravel_index(self.np_random.multinomial(1, self.O[self.state, (*actions), state1].flatten()).argmax(),
+                               tuple([oSpace.n for oSpace in self.observation_space]))
+        reward = self.R[self.state, (*actions), state1, (*obs)].item()
+
+        if self.episodic:
+            done = self.D[self.state, (*actions)]
+        else:
+            done = False
+
+        if done:
+            self.state = None
+        else:
+            self.state = state1
+
+        return obs, reward, done, {}
+
+    # Some dpomdp files have rounding errors that mean probabilities don't sum to 1
+    # This fixes them
+    def _normalizeInit(self):
+        stateIndices = np.arange(self.state_space.n)
+        actionIndices = tuple([np.arange(aAct.n) for aAct in self.action_space])
+        self.T[stateIndices, (*actionIndices)] /= self.T[stateIndices, (*actionIndices)].sum()
+        self.O[stateIndices, (*actionIndices), stateIndices] /= self.O[stateIndices, (*actionIndices), stateIndices].sum()
+        
+
+
+
+
+if __name__=="__main__":
+    pathToDPOMDPs = 'DPOMDPs'
+    DPOMDPFileList = [os.path.join(pathToDPOMDPs, file) for file in os.listdir(pathToDPOMDPs)]
+    for dpom in DPOMDPFileList[1:]:
+        try:
+            testDPOMDP = DPOMDP(dpom, episodic=True)
+            testDPOMDP.reset()
+            testDPOMDP.step(np.arange(2))
+        except Exception as e:
+            print(dpom)
+            print(e)
