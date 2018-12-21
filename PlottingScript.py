d5efe00cbd73fd5dd10720e51fa5c9b8f092f21c
Unknown <david.slayback2010@gmail.com>
2018-12-21 08:52:16 -0500

    Removed the photo files from Yuchen's code, since their filenames caused issues on windows systems

--- PlottingScript.py
+++ PlottingScript.py
@@ -21,5 +21,16 @@ if __name__ == "__main__":
         np.nanmean(iterValues, axis=0), np.nanmean(iterStdDev, axis=0), np.nanstd(iterValues, axis=0), \
         (np.nanmin(iterValues, axis=0), np.nanmax(iterValues, axis=0))
 
+    for envIndex in range(meanVals.shape[0]):
+        pass
+        """
+        What should I plot for each environment?
+        Honestly, probably need to choose these params AND THEN do mean, std, range, etc calculations
+        
+        Effects of number of nodes on performance (for each num, plot performance averaged over all params with that num?)
+        Effects of number of samples
+        Effects of number of best samples
+        Effects of a positive value threshold? Would kinda depend on environment. Honestly, probably best to throw these out for the other params
+        """
 
     pass
