fc37e3412d4cb4d5fb35828499e0189fdc2e8998
David Slayback <david.slayback2010@gmail.com>
2019-02-20 11:53:24 -0500

    Added shell scripts for entropy injection variants and POMDP variants with new learning rates

--- ShellScripts/newOuterPOMDP.sh
+++ ShellScripts/newOuterPOMDP.sh
@@ -3,5 +3,5 @@
 fullDir="/scratch/slayback.d/GDICE/"
 for jobInd in {1..100..1}
 do
-    sbatch /scratch/slayback.d/GDICE/G-DICE/ShellScripts/newInner.sh "--save_path" $fullDir "--env_type" "POMDP" "--set_list" "POMDPsToEval.txt"
+    sbatch /scratch/slayback.d/GDICE/G-DICE/ShellScripts/newInner.sh "--save_path" $fullDir "--env_type" "POMDP" "--set_list" "NoEntPOMDPsToEval.txt"
 done
