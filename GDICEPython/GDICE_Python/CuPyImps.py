d857ad42e3db6ff207a650227193645bf94df817
Unknown <david.slayback2010@gmail.com>
2019-02-24 09:12:56 -0500

    cupy isn't epecially fast (handling multinomial call is the big deal here)

--- GDICEPython/GDICE_Python/CuPyImps.py
+++ GDICEPython/GDICE_Python/CuPyImps.py
@@ -1,5 +1,14 @@
 import cupy as cp
+import cupy.random as cpr
 import numpy as np
+import numpy.random as npr
+
+from gym_dpomdps import list_dpomdps
+from gym_pomdps import list_pomdps
+import gym
+
+from GDICE_Python.Controllers import FiniteStateControllerDistribution
+from GDICE_Python.Utils import sampleFromControllerDistribution
 
 # Extract gym fields so they can be used in a JIT function
 # T, O, R, nAgents, nActions, nObs, nStates, discount, start
@@ -16,7 +25,7 @@ def extractGymFieldsPOMDP(pomdp):
         nActions = pomdp.action_space.n
         nObs = pomdp.observation_space.n
     T, O, R = pomdp.T, pomdp.O, pomdp.R
-    T, O = normalizeProbTablesPOMDP(T, O)
+    #T, O = normalizeProbTablesPOMDP(T, O)
 
     return T, O, R, nAgents, nActions, nObs, nStates, discount, start
 
@@ -37,8 +46,67 @@ def getStartState(start, nTrajectories=1000):
 # Transfer environment to gpu first
 def sendGymToGPU(pomdp):
     T, O, R, nAgents, nActions, nObs, nStates, discount, start = extractGymFieldsPOMDP(pomdp)
-    T = cp.asarray(T)  # Transition matrix
-    O = cp.asarray(O)  # Observation transition matrix
-    R = cp.asarray(R)  # Reward matrix
-    start = cp.asarray(start)  # Start probability (for reset)
+    T = cp.array(T)  # Transition matrix
+    O = cp.array(O)  # Observation transition matrix
+    R = cp.array(R)  # Reward matrix
+    start = cp.array(start)  # Start probability (for reset)
     return T, O, R, start
+
+# Init stuff on GPU
+def initOnGPU(aTrans, ntTrans, startG, nSim=1000):
+    nN = aTrans.shape[0]
+    nSamples = aTrans.shape[1]
+
+    # Reset state and nodes
+    stateG = cp.zeros((nSamples, nSim), dtype=cp.int32)
+    currNodeG = cp.zeros((nSamples, nSim), dtype=cp.int32)
+    stateG = resetGymGPU(stateG, currNodeG, startG)
+
+    # Send new set of sampled transitions
+    aTransG = cp.array(aTrans)  # nN*nSamples
+    ntTransG = cp.array(ntTrans)  # nO*nN*nSamples
+
+    # Create things to store rewards, obs
+    newStateG = cp.zeros((nSamples, nSim), dtype=cp.int32)
+    obsG = cp.zeros((nSamples, nSim), dtype=cp.int32)
+    valueG = cp.zeros((nSamples, nSim), dtype=cp.float64)
+
+    # Convenience indices
+    sampIndG = cp.arange(nSamples)  # 0:50
+    nodeIndG = cp.arange(nN)
+
+    return stateG, currNodeG, aTransG, ntTransG, newStateG, obsG, valueG, sampIndG, nodeIndG
+
+
+# Reset state to start
+def resetGymGPU(stateG, currNodeG, startG):
+    stateG = cpr.multinomial(1, startG, size=(50,1000)).argmax(2)
+    currNodeG.fill(0)
+    return stateG
+
+def resetIntermediateGPU(valueG):
+    valueG.fill(0.0)
+
+def stepGPU(stateG, newStateG, tG, oG, rG, currNodeG, aTransG, ntTransG, gamma, sampIndG, nodeIndG):
+    nSim = stateG.shape[-1]
+    nSamp = stateG.shape[0]
+    fullSampIndices = cp.tile(cp.expand_dims(sampIndG, 1), (1, nSim))
+    actionsG = aTransG[currNodeG, fullSampIndices]  #50*1000 actions at this timestep for each sample-sim combination
+    #newStateG = cpr.multinomial(1, tG[s, a]) for s
+    test = tG[stateG, actionsG]
+    pass
+    #np.array([cpr.multinomial(1, p).argmax() for p in tG[stateG, aTransG]])
+
+
+
+if __name__ == "__main__":
+    env = gym.make('POMDP-4x3-episodic-v0')
+    tH = 100
+    controllerDistribution = FiniteStateControllerDistribution(10, env.action_space.n, env.observation_space.n)
+    tG, oG, rG, startG = sendGymToGPU(env)
+    aTrans, ntTrans = sampleFromControllerDistribution(controllerDistribution, 50)
+    stateG, currNodeG, aTransG, ntTransG, newStateG, obsG, valueG, sampIndG, nodeIndG = initOnGPU(aTrans, ntTrans, startG)
+    stepGPU(stateG, newStateG, tG, oG, rG, currNodeG, aTransG, ntTransG, 0.99, sampIndG, nodeIndG)
+    for t in range(tH):
+
+        pass
