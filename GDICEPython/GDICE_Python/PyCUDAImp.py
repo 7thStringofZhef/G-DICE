827b9800b795eeddf4aa537343d7b8827b6ac070
Unknown <david.slayback2010@gmail.com>
2019-02-24 12:28:08 -0500

    And pyCuda's a pain too...and so is C++

--- GDICEPython/GDICE_Python/PyCUDAImp.py
+++ GDICEPython/GDICE_Python/PyCUDAImp.py
@@ -2,3 +2,13 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
+
+# 1 block per sample
+# nSim threads per block
+multinomialTMod = SourceModule("""
+  __global__ void sampleT(float *a)
+  {
+    int idx = threadIdx.x + threadIdx.y*4;
+    a[idx] *= 2;
+  } 
+""")
