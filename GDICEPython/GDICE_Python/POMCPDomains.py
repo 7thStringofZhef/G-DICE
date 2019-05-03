8019f3f5b0c9c56437a851bfd56039274eb2998f
David Slayback <david.slayback2010@gmail.com>
2019-05-03 11:13:38 -0400

    Battleship works

--- GDICEPython/GDICE_Python/POMCPDomains.py
+++ GDICEPython/GDICE_Python/POMCPDomains.py
@@ -9,9 +9,10 @@ from gym.spaces import Discrete
 from gym.utils import seeding
 
 from functools import partial
+from itertools import chain
 
-from gym_pomdp.envs.coord import Grid, Coord
-from gym_pomdp.envs.gui import ShipGui
+#from gym_pomdp.envs.coord import Grid, Coord
+#from gym_pomdp.envs.gui import ShipGui
 
 # Battleship
 # 1 agent
@@ -23,13 +24,15 @@ from gym_pomdp.envs.gui import ShipGui
 # State of each cell: 0 (empty), 1 (empty, visited), 2 (empty, diagonal), 3 (occupied), 4 (occupied, visited),
 
 class BattleshipPOMDP(POMDP):
-    def __init__(self, boardSize=10, seed=None):
+    def __init__(self, boardSize=10, seed=None, prune=True):
         self.seed(seed)
+        self.prune = prune
         self.boardSize = boardSize
         self.observation_space = Discrete(2)  # Miss or hit
         self.action_space = Discrete(boardSize ** 2)  # Fire on any square
         self.actionMap = partial(np.unravel_index, dims=(boardSize, boardSize))  # Function to get flat indices from tuple
         self.actionUnMap = partial(np.ravel_multi_index, dims=(boardSize, boardSize))  # Function to get tuple indices from flat
+        self.reset()
 
     # Seed the RNG
     def seed(self, seed):
@@ -37,238 +40,104 @@ class BattleshipPOMDP(POMDP):
         return [seed_]
 
     def reset(self):
-        self.grid = np.zeros(self.boardSize ** 2, dtype=np.uint8)  # Grid (as flat)
+        # Occupied, visited, diagonal
+        self.occupied = np.zeros(self.boardSize ** 2, dtype=bool)
+        self.visited = np.zeros(self.boardSize ** 2, dtype=bool)
+        self.diagonal = np.zeros(self.boardSize ** 2, dtype=bool)
+
         self.legalMoves = np.ones(self.boardSize ** 2, dtype=bool)  # Which squares have been fired on? Mark those as false
         self.preferredMoves = np.ones(self.boardSize ** 2, dtype=bool)  # Additionally prune squares diagonal to hit
         self._initShips()
 
     # Place a valid configuration of ships on the board
     def _initShips(self):
-        shipLengths = [5, 4, 3, 3, 2]  # Carrier, battleship, cruiser, sub, destroyer
-        positions = self.actionMap(np.arange(self.boardSize ** 2))  # Flat indices 0-100
+        self.invCheck = lambda pos: self.occupied[pos] or self.diagonal[pos]  # Check square occupied/adjacency status
+        self.shipLengths = [5, 4, 3, 3, 2]  # Carrier, battleship, cruiser, sub, destroyer
+        self.numSegments = np.sum(self.shipLengths)
+        positions = np.arange(self.boardSize ** 2)  # Flat indices 0-100
         directions = np.arange(4)  # Directions
         permutations = np.array(np.meshgrid(positions, directions)).reshape(2, -1).swapaxes(0, 1)
-        for ship in shipLengths:
-            shuffleInd = self.np_random.permutation(directions.size * positions.size)  # Shuffle permutations
+        shuffleInd = self.np_random.permutation(directions.size * positions.size)  # Shuffle permutations
+        for ship in self.shipLengths:
             perm = permutations[shuffleInd, :]
             for pos, dir in perm:
-                if not self._collide(pos, dir, ship):
-                    self._placeShip(pos, dir, ship)
+                valid = self._valid(pos, dir, ship)
+                if valid:
+                    self._placeShip(valid)
                     break
 
     # Does placing this new ship collide with previous ships? Or with the grid?
     # shipPos is flat index
-    # shipDir is 0 (N), 1 (E), 2 (W), 3 (S)
-    def _collide(self, shipPos, shipDir, shipLength):
-        posAsTuple = self.actionUnMap(shipPos)
-        allPosAsTuple = [posAsTuple]
+    # shipDir is 0 (N), 1 (E), 2 (S), 3 (W)
+    def _valid(self, shipPos, shipDir, shipLength):
+        if self.invCheck(shipPos): return []
+        shipPosAsTuple = self.actionMap(shipPos)
+        allPos = [shipPosAsTuple]
+        # Get all positions (reverse so I run into out-of-bounds errors faster)
+        for i in reversed(range(1, shipLength)):
+            if shipDir == 0:  # North
+                newTup = (shipPosAsTuple[0] - i, shipPosAsTuple[1])
+                if newTup[0] < 0 or self.invCheck(self.actionUnMap(newTup)): return []
+                else: allPos.append(newTup)
+            elif shipDir == 1:  # East
+                newTup = (shipPosAsTuple[0], shipPosAsTuple[1] + i)
+                if newTup[1] >= self.boardSize or self.invCheck(self.actionUnMap(newTup)): return []
+                else: allPos.append(newTup)
+            elif shipDir == 2:  # South
+                newTup = (shipPosAsTuple[0] + i, shipPosAsTuple[1])
+                if newTup[0] >= self.boardSize or self.invCheck(self.actionUnMap(newTup)): return []
+                else: allPos.append(newTup)
+            elif shipDir == 3:  # West
+                newTup = (shipPosAsTuple[0], shipPosAsTuple[1] - i)
+                if newTup[1] < 0 or self.invCheck(self.actionUnMap(newTup)): return []
+                else: allPos.append(newTup)
+        return allPos
 
+    # Place ship according to a set of tuple coordinates
+    def _placeShip(self, validPosition):
+        self.occupied[[self.actionUnMap(p) for p in validPosition]] = True
+        diagonalPos = list(set(chain.from_iterable([self._diagOfPos(valPos) for valPos in validPosition])))  # Convert to a set to make unique
+        # Remove invalid indices
+        diagonalPos = [pos for pos in diagonalPos if 0 <= pos[0] < self.boardSize and 0 <= pos[1] < self.boardSize]
+        self.diagonal[[self.actionUnMap(p) for p in diagonalPos]] = True
 
-        return False
+    def _diagOfPos(self, valPos):
+        return [(valPos[0] - 1, valPos[1] - 1), (valPos[0] - 1, valPos[1] + 1), (valPos[0] + 1, valPos[1] + 1), (valPos[0] + 1, valPos[1] - 1)]
 
-    def _placeShip(self, shipPos, shipDir, shipLength):
-        allPos = 0
-
-
-
-# TODO fix state
-class Compass(Enum):
-    North = Coord(0, 1)
-    East = Coord(1, 0)
-    South = Coord(0, -1)
-    West = Coord(-1, 0)
-    Null = Coord(0, 0)
-    NorthEast = Coord(1, 1)
-    SouthEast = Coord(1, -1)
-    SouthWest = Coord(-1, -1)
-    NorthWest = Coord(-1, 1)
-
-    @staticmethod
-    def get_coord(idx):
-        return list(Compass)[idx].value
-
-
-class Obs(Enum):
-    NULL = 0
-    HIT = 1
-
-
-class Ship(object):
-    def __init__(self, coord, length):
-        self.pos = coord
-        self.direction = np.random.randint(4)
-        self.length = length
-
-
-class ShipState(object):
-    def __init__(self):
-        self.ships = []
-        self.total_remaining = 0
-
-
-class Cell(object):
-    def __init__(self):
-        self.occupied = False
-        self.visited = False
-        self.diagonal = False
-
-
-class BattleGrid(Grid):
-    def __init__(self, board_size):
-        super().__init__(*board_size)
-
-    def build_board(self, value=0):
-        self.board = []
-        for idx in range(self.n_tiles):
-            self.board.append(Cell())
-        self.board = np.asarray(self.board).reshape((self.x_size, self.y_size))
-
-
-class BattleShipEnv(Env):
-    metadata = {"render.modes": ["human", "ansi"]}
-
-    def __init__(self, board_size=(5, 5), max_len=3):
-        self.grid = BattleGrid(board_size)
-        self.action_space = Discrete(self.grid.n_tiles)
-        self.observation_space = Discrete(len(Obs))
-        self.num_obs = 2
-        self._reward_range = self.action_space.n / 4.
-        self._discount = 1.
-        self.total_remaining = max_len - 1
-        self.max_len = max_len + 1
-
-    def seed(self, seed=None):
-        np.random.seed(seed)
-
-    def _compute_prob(self, action, next_state, ob):
-
-        action_pos = self.grid.get_coord(action)
-        cell = self.grid[action_pos]
-        if ob == Obs.NULL.value and cell.visited:
-            return 1
-        elif ob == Obs.HIT.value and cell.occupied:
-            return 1
-        else:
-            return int(ob == Obs.NULL.value)
-
+    # Fire a shot
     def step(self, action):
-
-        assert self.done is False
-        assert self.action_space.contains(action)
-        assert self.total_remaining > 0
-        self.last_action = action
-        self.t += 1
-        action_pos = self.grid.get_coord(action)
-        # cell = self.grid.get_value(action_pos)
-        cell = self.grid[action_pos]
-        reward = 0
-        if cell.visited:
-            reward -= 10
-            obs = Obs.NULL.value
+        refire = False
+        # Visit the square
+        if self.occupied[action] and not self.visited[action]:
+            obs = 1
         else:
-            if cell.occupied:
-                reward -= 1
-                obs = 1
-                self.state.total_remaining -= 1
+            if self.visited[action]: refire = True  # Are we shooting the same square again?
+            obs = 0
+        self.visited[action] = True
+        r = self._rewardFn()
+        isDone = self._isDone()
 
-                for d in range(4, 8):
-                    if self.grid[action_pos + Compass.get_coord(d)]:
-                        self.grid[action_pos + Compass.get_coord(d)].diagonal = False
-            else:
-                reward -= 1
-                obs = Obs.NULL.value
-            cell.visited = True
-        if self.state.total_remaining == 0:
-            reward += self.grid.n_tiles
-            self.done = True
-        self.tot_rw += reward
-        return obs, reward, self.done, {"state": self.state}
+        # Update set of legal moves
+        if self.prune and not refire:
+            self.legalMoves[action] = False
+            self.preferredMoves[action] = False
+            if obs:  # If a hit, can't be any of the diagonals
+                actionAsTup = self.actionMap(action)
+                diagonalPos = self._diagOfPos(actionAsTup)
+                diagonalPos = [pos for pos in diagonalPos if 0 <= pos[0] < self.boardSize and 0 <= pos[1] < self.boardSize]
+                self.preferredMoves[[self.actionUnMap(p) for p in diagonalPos]] = False
+        return obs, r, isDone
 
-    def _set_state(self, state):
-        self.reset()
-        self.state = state
+    # +100 if you win. -1 for everything else
+    def _rewardFn(self):
+        return 100 if np.sum(np.logical_and(self.visited, self.occupied)) == self.numSegments else -1
 
-    def close(self):
-        return
+    # Done if you win
+    def _isDone(self):
+        return np.sum(np.logical_and(self.visited, self.occupied)) == self.numSegments
 
-    def reset(self):
-        self.done = False
-        self.tot_rw = 0
-        self.t = 0
-        self.last_action = -1
-        self.state = self._get_init_state()
-        return Obs.NULL.value
 
-    def render(self, mode='human', close=False):
-        if close:
-            return
-        if mode == 'human':
-            if not hasattr(self, "gui"):
-                obj_pos = []
-                for ship in self.state.ships:
-                    pos = ship.pos
-                    obj_pos.append(self.grid.get_index(pos))
-                    for i in range(ship.length):
-                        pos += Compass.get_coord(ship.direction)
-                        obj_pos.append(self.grid.get_index(pos))
-                self.gui = ShipGui(board_size=self.grid.get_size, obj_pos=obj_pos)
-            if self.t > 0:
-                msg = "A: " + str(self.grid.get_coord(self.last_action)) + "T: " + str(self.t) + "Rw :" + str(
-                    self.tot_rw)
-                self.gui.render(state=self.last_action, msg=msg)
-
-    def _generate_legal(self):
-        # assert self.state.total_remaining > 0
-        actions = []
-        for action in range(self.action_space.n):
-            action_pos = self.grid.get_coord(action)
-            if not self.grid[action_pos].visited:
-                actions.append(action)
-        # assert len(actions) > 0
-        return actions
-
-    def _get_init_state(self):
-        bsstate = ShipState()
-        self.grid.build_board()
-
-        for length in reversed(range(2, self.max_len)):
-            # num_ships = 1
-            # for idx in range(num_ships):
-            while True:  # add one ship of each kind
-                ship = Ship(coord=self.grid.sample(), length=length)
-                if not self.collision(ship, self.grid, bsstate):
-                    break
-            self.mark_ship(ship, self.grid, bsstate)
-            bsstate.ships.append(ship)
-        return bsstate
-
-    @staticmethod
-    def mark_ship(ship, grid, state):
-
-        pos = ship.pos  # .copy()
-
-        for i in range(ship.length):
-            cell = grid[pos]
-            assert not cell.occupied
-            cell.occupied = True
-            if not cell.visited:
-                state.total_remaining += 1
-            pos += Compass.get_coord(ship.direction)
-
-    @staticmethod
-    def collision(ship, grid, state):
-
-        pos = ship.pos  # .copy()
-        for i in range(ship.length):
-            if not grid.is_inside(pos + Compass.get_coord(ship.direction)):
-                return True
-            # cell = grid.get_value(pos)
-            cell = grid[pos]
-            if cell.occupied:
-                return True
-            for adj in range(8):
-                coord = pos + Compass.get_coord(adj)
-                if grid.is_inside(coord) and grid[coord].occupied:
-                    return True
-            pos += Compass.get_coord(ship.direction)
-        return False
+if __name__ == "__main__":
+    test = BattleshipPOMDP()
+    test2 = test.step(0)
+    pass
